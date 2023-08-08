fb2a759136641070b6df7b7362fde2ef2579b115
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-08-08 20:23:45 +0600

    Implement With Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {MessagesComponent} from './messages/messages.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
@@ -30,7 +30,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     FormsModule,
     HttpClientInMemoryWebApiModule.forRoot(
         InMemoryDataService, {dataEncapsulation: false}
-    )
+    ),
+    ReactiveFormsModule
   ],
   providers: []
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HeroService} from '../hero.service';
 import {Hero} from "../hero";
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
@@ -17,18 +18,26 @@ export class HeroesComponent implements OnInit{
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-        .subscribe(hero => {
-          this.heroes.push(hero);
-        });
-  }
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+
+  applyForm = new FormGroup({
+    name: new FormControl(''),
+    dob: new FormControl(''),
+    description: new FormControl('')
+  });
+  addWithReactiveForm() {
+    let name = (this.applyForm.value.name ?? '').trim();
+    let dob = (this.applyForm.value.dob ?? '').trim();
+    let description = (this.applyForm.value.description ?? '').trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name, dob, description } as Hero)
+        .subscribe(hero => {
+          this.heroes.push(hero);
+        });
   }
 
 }
