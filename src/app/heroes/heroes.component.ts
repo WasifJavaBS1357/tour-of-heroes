5201af60ba1814b9710eeadf4cbbdd4545376f65
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-08-08 17:18:48 +0600

    Add Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,15 @@
-import { Component } from '@angular/core';
-import { HeroService } from '../hero.service';
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from '../hero.service';
 import {Hero} from "../hero";
-import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
+  constructor(private heroService: HeroService) { }
   ngOnInit(): void {
     this.getHeroes();
   }
