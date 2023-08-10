5b26ac734308871f54e3e97dc5e13e8983020091
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-08-10 19:41:16 +0600

    Implement reactive form for update

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,7 +3,7 @@ import {Hero} from '../hero';
 import {ActivatedRoute} from "@angular/router";
 import {HeroService} from "../hero.service";
 import {Location} from '@angular/common';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-hero-detail',
@@ -13,37 +13,44 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class HeroDetailComponent {
 
   hero: Hero | undefined;
+  applyForm: FormGroup | undefined;
   constructor(
       private route: ActivatedRoute,
       private heroService: HeroService,
-      private location: Location
-  ) {}
-
-  ngOnInit(): void {
-    this.getHero();
+      private fb: FormBuilder,
+      private location: Location,
+  ) {
   }
 
-  applyForm = new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    dob: new FormControl(''),
-    description: new FormControl('')
-  });
+  initForm():void{
+    // console.log(this.applyForm);
+    this.applyForm = this.fb.group({
+      name: new FormControl(''),
+      dob: new FormControl(''),
+      description: new FormControl('')
+    });
+  }
+  ngOnInit(): void {
+    this.getHero();
+    this.initForm();
+  }
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
-        .subscribe(hero => this.hero = hero);
-  }
-  save(): void {
-    if (this.hero) {
-      this.heroService.updateHero(this.hero)
-          .subscribe(() => this.goBack());
-    }
+      .subscribe(hero => {
+          this.hero = hero;
+          this.applyForm?.patchValue({
+            dob:this.hero?.dob,
+            name: this.hero?.name,
+            description: this.hero?.description
+          })
+      })
   }
 
   saveWithReactiveFrom(): void{
     console.log("hello");
-    const { name, dob, description } = this.applyForm.value;
+    const { name, dob, description } = this.applyForm?.value;
     const id = this.hero?.id;
     this.heroService.updateHero({ id , name, dob, description } as Hero)
       .subscribe(() => this.goBack());
